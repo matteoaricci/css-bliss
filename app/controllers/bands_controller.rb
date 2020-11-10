5b0960d256aaa5ec89cfc233193d1eb4f991b717
matteoaricci <matteo.a.ricci@gmail.com>
2020-11-10 09:05:25 -0500

    final look

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,2 +1,12 @@
 class BandsController < ApplicationController
+    def index
+        @bands = Band.all
+    end
+
+    def show
+        @band = Band.find(params[:id])
+        @members = @band.band_members
+        @albums = @band.albums
+    end
+
 end
